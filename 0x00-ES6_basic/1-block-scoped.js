f3aedc47529c561ff0831aae861b038669250f06
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-23 00:30:39 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,13 @@
 export default function taskBlock(trueOrFalse) {
- /*eslint-disable */
- const task = false;
- const task2 = true;
+  /*eslint-disable */
+  const task = false;
+  const task2 = true;
 
  if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    task = true;
+    task2 = false;
  }
 
  return [task, task2];
- /* eslint-enable */
+  /* eslint-enable */
 }
