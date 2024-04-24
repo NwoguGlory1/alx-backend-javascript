8d3af14fb13dd1eeca28fa37902f4418075a1569
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-24 21:52:22 +0000

    Reject the promises

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,3 @@
 export default function uploadPhoto(filename) {
-// Return a Promise that rejects with an Error
-  return new Promise((reject) => {
-    reject(new Error(`<rejected> Error: ${filename} cannot be processed`));
-  });
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
