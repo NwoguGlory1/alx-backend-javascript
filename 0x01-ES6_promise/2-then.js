9c3d81b3e955753d973ef123d878dfef955ec5b5
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-24 15:24:58 +0000

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,10 @@
 // Function appending handlers to a promise
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch(() => new Error()); // Reject with an empty Error object
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
