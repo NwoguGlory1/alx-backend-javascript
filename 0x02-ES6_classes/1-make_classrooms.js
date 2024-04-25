d0aea325a677938e8143c1ed7d595030a72272d2
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-25 22:18:35 +0000

    Let's make some classrooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,22 @@
+// Import the ClassRoom class
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  const arrayy = [];
+
+  const arrayy1 = new ClassRoom(19);
+  arrayy.push(arrayy1);
+
+  const arrayy2 = new ClassRoom(20);
+  arrayy.push(arrayy2);
+
+  const arrayy3 = new ClassRoom(34);
+  arrayy.push(arrayy3);
+  return arrayy;
+}
+
+// Call the function to create an array
+const Rooms = initializeRooms();
+
+// Output the array of ClassRoom objects
+console.log(Rooms);
