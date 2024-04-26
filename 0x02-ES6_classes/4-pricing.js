88c7fdc6548526d4015fa92c57d533b6e5a2d82f
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-26 02:41:02 +0000

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+// Import the ClassRoom class
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter method for code property
+  get amount() {
+    return this._amount;
+  }
+
+  // Setter method for code property
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // Getter method for name property
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter method for name property
+  set name(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Method to display the full currency information
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name}  (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
