6594a8295204c8cf3cc62be3cd1283459778f866
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-26 16:50:57 +0000

    Iterator

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,9 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const result = {};
-  result[departmentName] = employees;
+// Create an object with the departmentName as the key and employees as the value
+  const result = {
+    [departmentName]: employees,
+  };
+
+  // Return the created object
   return result;
 }
