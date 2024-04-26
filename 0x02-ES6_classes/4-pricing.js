f165a98d3eaad36763c66d999cc17d984b163ec1
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-26 03:10:58 +0000

    Pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -3,33 +3,33 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
-  // Getter method for code property
+  // Getter method for amount property
   get amount() {
     return this._amount;
   }
 
-  // Setter method for code property
+  // Setter method for amount property
   set amount(newAmount) {
-    this._amount = newAmount;
+    if (typeof newAmount === 'number') this._amount = newAmount;
   }
 
-  // Getter method for name property
+  // Getter method for currency property
   get currency() {
     return this._currency;
   }
 
-  // Setter method for name property
-  set name(newCurrency) {
-    this._currency = newCurrency;
+  // Setter method for currency  property
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) this._currency = newCurrency;
   }
 
   // Method to display the full currency information
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name}  (${this._currency.code})`;
+    return `${this.amount} ${this.currency.name}  (${this.currency.code})`;
   }
 
   // Static method to convert price
