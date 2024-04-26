275d71d6e047fcb7e16289ec1657adb4d4bfd361
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-26 03:16:59 +0000

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,64 +1,49 @@
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  // Getter method for name property
+  // name
   get name() {
     return this._name;
   }
 
-  // Setter method for name property
-  set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
     } else {
       throw new TypeError('Name must be a string');
     }
   }
 
-  // Getter method for length property
+  // length
   get length() {
     return this._length;
   }
 
-  // Setter method for length property
-  set length(newLength) {
-    if (typeof newLength === 'number') {
-      this._length = newLength;
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
     } else {
       throw new TypeError('Length must be a number');
     }
   }
 
-  // Getter method for students property
+  // students
   get students() {
     return this._students;
   }
 
-  // Setter method for students property
-  set students(newStudents) {
+  set students(students) {
     if (
-      Array.isArray(newStudents) && newStudents.every((student) => typeof student === 'string')
+      Array.isArray(students)
+      && students.every((s) => typeof s === 'string')
     ) {
-      this._students = newStudents;
+      this._students = students;
     } else {
       throw new TypeError('Students must be an array of strings');
     }
   }
-}
-
-// Create a new HolbertonCourse object
-try {
-  const course = new HolbertonCourse('JavaScript', 12, ['Alice', 'Bob', 'Charlie']);
-  console.log(course);
-
-  // Attempt to update properties with invalid values
-  course.name = 123; // Invalid value for name
-  course.length = -5; // Invalid value for length
-  course.students = ['Alice', 123, 'Charlie']; // Invalid value for students
-} catch (error) {
-  console.error('Error:', error.message);
 }
