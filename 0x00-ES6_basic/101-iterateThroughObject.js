4765c9e3e9349ec55e0ade43ae436cca22f5c29f
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-26 18:20:15 +0000

    Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,15 +1,8 @@
 export default function iterateThroughObject(reportWithIterator) {
- let employeeNames = [];
+  const employees = [];
+  for (const r of reportWithIterator) {
+    employees.push(r);
+  }
 
- // Iterate over each department in the reportWithIterator object
- for (let department in reportWithIterator) {
-    // For each department, iterate over the array of employee names
-    for (let employee of reportWithIterator[department]) {
-      employeeNames.push(employee);
-    }
- }
-
- // Join the employee names with ' | ' and return the resulting string
- return employeeNames.join(' | ');
+  return employees.join(' | ');
 }
-
