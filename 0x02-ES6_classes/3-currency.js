cfd9ccb3e27258ee2e9f2705c212f9ce99c176c3
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-26 01:56:07 +0000

    Methods, static methods, computed methods names..... MONEY

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -2,28 +2,30 @@ export default class Currency {
   constructor(code, name) {
     this._code = code;
     this._name = name;
+  }
 
-// Getter method for code property
-get code() {
-  return this._code;
-}
+  // Getter method for code property
+  get code() {
+    return this._code;
+  }
 
-// Setter method for code property
-set code(code) {
-  return this._code = code;
-}
+  // Setter method for code property
+  set code(newCode) {
+    this._code = newCode;
+  }
 
+  // Getter method for name property
+  get name() {
+    return this._name;
+  }
 
-// Getter method for name property
-get name() {
-  return this._name;
-}
+  // Setter method for name property
+  set name(newName) {
+    this._name = newName;
+  }
 
-// Setter method for name property
-set name(name) {
-  this._name = name;
-}
-
-  export default displayFullCurrency() {
+  // Method to display the full currency information
+  displayFullCurrency() {
     return `${this._name} (${this._code}₦)`;
+  }
 }
