b3b210dd13cabb69195dc5dbc1220f95b1f4fa82
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-30 15:29:04 +0000

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,18 +1,18 @@
 export default function cleanSet(set, startString) {
-  // Use the map method to transform each element of the Set
-  const transformedSet = Array.from(set).map((value) => {
-    // Check if the value starts with the startString
-    if (value.startsWith(startString)) {
-      // If it does, return the part of the string that comes after the startString
-      return value.substring(startString.length);
-    }
-    // If it doesn't, return an empty string (or any other value you prefer)
+  const string = [];
+
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
     return '';
-  });
+  }
 
-  // Use join method to combine all transformed elements into a single string,separated by dashes
-  const resultString = transformedSet.join('-');
-
-  // Return the resulting string
-  return resultString;
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      string.push(item.slice(startString.length));
+    }
+  }
+  return string.join('-');
 }
