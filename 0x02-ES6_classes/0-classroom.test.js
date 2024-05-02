28ec9cf0523a6c94374a03fd0eedba544cc72768
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-02 03:07:48 +0000

    Tets file

--- 0x02-ES6_classes/0-classroom.test.js
+++ 0x02-ES6_classes/0-classroom.test.js
@@ -1,8 +1,14 @@
-import ClassRoom from '../0-classroom.js';
+// Import the ClassRoom class
+import ClassRoom from './0-classroom';
 
+// Test suite for the ClassRoom class
 describe('ClassRoom', () => {
-  test('constructor should initialize _maxStudentsSize property', () => {
+ // Test case for the constructor
+ it('should correctly set the maxStudentsSize property', () => {
+    // Create a new instance of ClassRoom with maxStudentsSize set to 10
     const room = new ClassRoom(10);
+
+    // Expect the _maxStudentsSize property of the room instance to be 10
     expect(room._maxStudentsSize).toBe(10);
-  });
+ });
 });
