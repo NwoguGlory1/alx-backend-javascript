2639df36a1eb98096b8851d27e46bf7039e6b0fa
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-17 00:38:20 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,22 @@
+// Set the encoding for stdin to 'utf8' so it reads data as a string
 process.stdin.setEncoding('utf8');
+
+// Write the initial prompt to the standard output
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// Handle 'data' event to capture user input
 process.stdin.on('data', (data) => {
+  // Trim the data to remove any extraneous whitespace or newline characters
   const INPUT = data.trim();
+
+  // Display the user's name
   process.stdout.write(`Your name is: ${INPUT}\n`);
+
+  // End the input stream
   process.stdin.end();
 });
+
+// Handle 'end' event to display closing message
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
