5b2bc498d577b7a495253ba4d85cb1dd563fa897
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-20 02:33:33 +0000

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,25 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
+
+// Describe the "Calculator" feature or module
 describe('Calculator', () => {
+  // Describe the "calculateNumber" function
   describe('calculateNumber()', () => {
+  // Test case description
   it('should round each input number and return their sum', () => {
+
     // Test case 1: Adding two positive integers
     const result = calculateNumber(1.23, 4.56);
     assert.equal(result, 6);
-   // Test case 2: Adding two negative integers
+
+    // Test case 2: Adding two negative integers
     const resultNegative = calculateNumber(-1.23, -4.56);
     assert.equal(resultNegative, -6);
+
     // Test case 3: Adding a positive and a negative integer
     const resultMixed = calculateNumber(1.23, -4.56);
     assert.equal(resultMixed, -4);
+
     // Test case 4: Rounding to zero
     const resultZero = calculateNumber(0.99, 0.01);
     assert.equal(resultZero, 1);
