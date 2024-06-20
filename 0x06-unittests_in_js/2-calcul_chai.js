7f1fdd6cdcc04804c78d7fbe7bcda070ac54cca3
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-20 05:39:24 +0000

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,25 @@
+function calculateNumber(type, a, b) {
+  if (typeof type !== 'string') {
+    throw new Error('Type must be a string');
+  }
+
+  // Round the numbers
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+
+  // Perform the operation based on the type
+  if (type === 'SUM') {
+    return roundedA + roundedB;
+  } else if (type === 'SUBTRACT') {
+    return roundedA - roundedB;
+  } else if (type === 'DIVIDE') {
+    if (roundedB === 0) {
+      return 'Error';
+    }
+    return roundedA / roundedB;
+  } else {
+    throw new Error('Invalid type');
+  }
+}
+
+module.exports = calculateNumber;
