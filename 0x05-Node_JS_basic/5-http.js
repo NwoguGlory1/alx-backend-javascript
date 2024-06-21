089f2d146588ac6bad250e5ccec69be70ec59f87
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-21 18:58:13 +0000

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -10,23 +10,43 @@ const app = http.createServer((req, res) => {
   // Set the Content-Type header to text/plain
   res.setHeader('Content-Type', 'text/plain');
 
-  const { url } = req;
+  const { url } = req; // Destructure 'url' from 'req' object
 
   if (url === '/') {
+    // If the URL is '/', respond with 'Hello Holberton School!'
     res.end('Hello Holberton School!');
   } else if (url === '/students') {
-    const database = process.argv[2]; // Get the database file path from command-line arguments
-    countStudents(database).then((response) => {
-      res.write('This is the list of our students\n');
-      res.end(response.join('\n'));
-    }).catch((error) => {
-      res.end(`Error: ${error.message}`);
-    });
+    // If the URL is '/students', get the database file path from command-line arguments
+    const database = process.argv[2];
+
+    // Check if the database argument is provided
+    if (!database) {
+      res.end('This is the list of our students\nError: No database file provided');
+      return;
+    }
+
+    // Call countStudents function and handle the response or error
+    countStudents(database)
+      .then((response) => {
+        // If successful, respond with the list of students
+        res.write('This is the list of our students\n');
+        res.end(response.join('\n'));
+      })
+      .catch((error) => {
+        // If there's an error, respond with the error message
+        res.end(`Error: ${error.message}`);
+      });
+  } else {
+    // If the URL is not recognized, respond with a 404 Not Found message
+    res.statusCode = 404;
+    res.end('404 Not Found');
   }
 });
 
+// Make the server listen on port 1245 and log a message when it's running
 app.listen(1245, 'localhost', () => {
   console.log('Server running at http://localhost:1245/');
 });
 
+// Export the server as 'app'
 module.exports = app;
