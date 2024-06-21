6b179a5b3170da858b98e7fcdf5a1e9a787952fe
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-21 00:52:11 +0000

    Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,32 @@
+// Import chai, sinon, Utils, sendPaymentRequestToApi
+const sinon = require('sinon');
+var { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function() {
+  // Define a test case
+  it('has the same math', () => {
+    // Create a stub for Utils.calculateNumber
+    // pass Utils, calculateNumber as arg
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    calculateNumberStub.returns(10);
+
+    // Create a spy for console.log
+    const consoleSpy = sinon.spy(console, 'log');
+
+    // Call the function being tested that uses Utils.calculateNumber
+    sendPaymentRequestToApi(100, 20);
+
+    // Verify the stub was called with the expected arguments
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+    // Verify that console.log was called with the correct message
+    expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+
+    // Restore the original function to remove the spy and stub
+    calculateNumberStub.restore();
+    consoleSpy.restore();
+  });
+});
