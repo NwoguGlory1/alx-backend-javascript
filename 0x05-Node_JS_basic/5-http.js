6dc350e8c0b4d93aa6ac1982dd9db273705087c7
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-21 18:40:53 +0000

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,36 +1,32 @@
+// Import the http module
 const http = require('http');
 
-const args = process.argv.slice(2);
+// Import the countStudents function
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = args[0];
-
-const hostname = '127.0.0.1';
-const port = 1245;
-
-const app = http.createServer(async (req, res) => {
+// Create the server
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
+  // Set the Content-Type header to text/plain
   res.setHeader('Content-Type', 'text/plain');
 
-  const { url } = req;
+  const url = req.url;
 
   if (url === '/') {
-    res.write('Hello Holberton School!');
+    res.end('Hello Holberton School!');
   } else if (url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
-    }
+    const database = process.argv[2]; // Get the database file path from command-line arguments
+    countStudents(database).then((response) => {
+      res.write('This is the list of our students\n');
+      res.end(response.join('\n'));
+    }).catch((error) => {
+      res.end(`Error: ${error.message}`);
+    });
   }
-  res.statusCode = 404;
-  res.end();
 });
 
-app.listen(port, hostname, () => {
-  //   console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(1245, 'localhost', () => {
+  console.log('Server running at http://localhost:1245/');
 });
 
 module.exports = app;
