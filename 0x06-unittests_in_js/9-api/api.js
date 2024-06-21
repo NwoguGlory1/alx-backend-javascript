c6d4fd347802fc0305189c92c17a2413d2b4c8ef
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-21 02:17:37 +0000

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.end('Welcome to the payment system');
 });
 
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.end(`Payment methods for cart ${req.params.id}`);
+});
+
 app.listen(7865, () => {
   console.log('API available on localhost port 7865');
 });
