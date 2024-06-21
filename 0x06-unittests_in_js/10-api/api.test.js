2418e52fcc198b6881196395b3cd6d448f8ba6b9
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-21 03:16:35 +0000

    Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,20 +1,22 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('Basic Integration Testing', function() {
-  describe('GET /', function() {
+describe('Integration Testing', () => {
+  describe('GET /', () => {
     it('Code: 200 | Body: Welcome to the payment system', (done) => {
       const options = {
         url: 'http://localhost:7865',
         method: 'GET',
       };
+
       request(options, function (error, response, body) {
         expect(response.statusCode).to.equal(200);
         expect(body).to.equal('Welcome to the payment system');
-	done();
+        done();
       });
     });
   });
+
   describe('GET /cart/12', () => {
     it('Responds with 200 and id 12 in msg', (done) => {
       const options = {
