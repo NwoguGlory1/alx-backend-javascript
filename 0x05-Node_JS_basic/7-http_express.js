349e8fa510507e9460427a317e7c610f493b04b1
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-22 13:02:29 +0000

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -13,7 +13,7 @@ const app = express();
 const port = 1245;
 
 // Define a route handler for the root path ('/')
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
   // Set the Content-Type header to text/plain
   res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
@@ -21,17 +21,17 @@ app.get('/', async(req, res) => {
 
 app.get('/students', async (req, res) => {
   if (!DATABASE) {
-  res.send('This is the list of our students\nError: No database file provided');
-  return;
+    res.send('This is the list of our students\nError: No database file provided');
+    return;
   }
 
   res.write('This is the list of our students\n');
-    try {
-      const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
-    }
+  try {
+    const students = await countStudents(DATABASE);
+    res.end(`${students.join('\n')}`);
+  } catch (error) {
+    res.end(error.message);
+  }
 });
 
 app.listen(port, () => {
